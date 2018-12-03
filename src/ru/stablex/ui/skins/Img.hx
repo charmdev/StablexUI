f78a563a79492c8999df70c92f05b110e7f94fdd
Zaphod <zaphy@yandex.ru>
2018-12-03 14:59:51 +0300

    drawing Img skin with drawTiles only

--- src/ru/stablex/ui/skins/Img.hx
+++ src/ru/stablex/ui/skins/Img.hx
@@ -20,23 +20,23 @@ class Img extends Skin
 	private static var matrix:Matrix = new Matrix();
 	
 	//Asset ID or path to bitmap
-	public var src (get,set): String;
-	public var _src : String = null;
-	/**
-	* Use this property instead of `.src`, if you need to directly assign BitmapData instance.
-	* `.bitmapData` will be set to null automatically, if you set `.src`.
-	* `.src` will be set to null automatically, if you set `.bitmapData`
-	*/
-	public var bitmapData (get,set) : BitmapData;
-	private var _bitmapData : BitmapData = null;
-	//Smooth bitmap?
-	public var smooth : Bool = true;
-	// If set to true, the img is scaled to the size of the widget. If false the widget is scaled to the size of the img.
-	public var scaleImg = false;
-	// If set to true and `scaleImg` is set to true, will maintain the aspect ratio of the image whilst scaling
-	public var keepAspect : Bool = false;
-	// If set to true as well as `scaleImg` and `keepAspect` the image will be cropped such that it fills the entire widget space
-	public var crop : Bool = false;
+    public var src (get_src,set_src): String;
+    public var _src : String = null;
+    /**
+    * Use this property instead of `.src`, if you need to directly assign BitmapData instance.
+    * `.bitmapData` will be set to null automatically, if you set `.src`.
+    * `.src` will be set to null automatically, if you set `.bitmapData`
+    */
+    public var bitmapData (get_bitmapData,set_bitmapData) : BitmapData;
+    private var _bitmapData : BitmapData = null;
+    //Smooth bitmap?
+    public var smooth : Bool = true;
+    // If set to true, the img is scaled to the size of the widget. If false the widget is scaled to the size of the img.
+    public var scaleImg = false;
+    // If set to true and `scaleImg` is set to true, will maintain the aspect ratio of the image whilst scaling
+    public var keepAspect : Bool = false;
+    // If set to true as well as `scaleImg` and `keepAspect` the image will be cropped such that it fills the entire widget space
+    public var crop : Bool = false;
 	
     /**
     * Draw skin
@@ -48,7 +48,7 @@ class Img extends Skin
 		var bmp:BitmapData = this._bitmapData;
 		var atlas:TextureAtlas = null;
 
-		if (bmp == null && this.src != null)
+        if (bmp == null && this.src != null)
 		{
 			atlas = com.nevosoft.isoframework.ResourceManager.instance.getAtlasOfRegion(this.src);
 			if (atlas == null)
@@ -60,8 +60,8 @@ class Img extends Skin
 					return;
 				}
 			}
-			
-		} else if (bmp == null)
+            
+        } else if (bmp == null)
 		{
             Err.trigger('Bitmap is not specified');
 			return;
@@ -69,6 +69,15 @@ class Img extends Skin
 		
 		if (atlas == null)
 		{
+#if wodrawrect 
+			var tilesheet = com.nevosoft.isoframework.ResourceManager.instance.getTilesheet(this.src);
+			if (tilesheet == null)
+			{
+				tilesheet = com.nevosoft.isoframework.ResourceManager.instance.registerTilesheet(this.src, bmp);
+				tilesheet.addTileRect(bmp.rect);
+			}
+#end
+			
 			//scale widget to image (default)
 			if (!scaleImg) 
 			{
@@ -77,10 +86,13 @@ class Img extends Skin
 					w.resize(bmp.width, bmp.height);
 				}
 				
+#if wodrawrect
+				w.graphics.drawTiles(tilesheet, [0.0, 0.0, 0], this.smooth);
+#else
 				w.graphics.beginBitmapFill(bmp, null, false, this.smooth);
 				w.graphics.drawRect(0, 0, bmp.width, bmp.height);
 				w.graphics.endFill();
-
+#end
 			//scale image to widget
 			} 
 			else 
@@ -102,7 +114,10 @@ class Img extends Skin
 				{
 					matrix.scale(scaleX, scaleY);
 				}
-
+				
+#if wodrawrect 
+				w.graphics.drawTiles(tilesheet, [0.0, 0.0, 0, scaleX, 0.0, 0.0, scaleY], this.smooth, Tilesheet.TILE_TRANS_2x2);
+#else
 				w.graphics.beginBitmapFill(bmp, matrix, false, this.smooth);
 				if (this.crop) 
 				{
@@ -114,6 +129,7 @@ class Img extends Skin
 				}
 				
 				w.graphics.endFill();
+#end
 			}
 		}
 		else
@@ -161,7 +177,7 @@ class Img extends Skin
 			}
 		}
 		
-	}//function draw()
+    }//function draw()
 	#else
 	override public function draw (w:Widget) : Void {
 		
@@ -178,14 +194,14 @@ class Img extends Skin
 			return;
         }
 
-		//scale widget to image (default)
-		if (!scaleImg) {
-			if( w.w != bmp.width || w.h != bmp.height ){
-				w.resize(bmp.width, bmp.height);
-			}
-			w.graphics.beginBitmapFill(bmp, null, false, this.smooth);
-			w.graphics.drawRect(0, 0, bmp.width, bmp.height);
-			w.graphics.endFill();
+        //scale widget to image (default)
+        if (!scaleImg) {
+            if( w.w != bmp.width || w.h != bmp.height ){
+                w.resize(bmp.width, bmp.height);
+            }
+            w.graphics.beginBitmapFill(bmp, null, false, this.smooth);
+            w.graphics.drawRect(0, 0, bmp.width, bmp.height);
+            w.graphics.endFill();
 
         //scale image to widget
         } else {
@@ -194,26 +210,26 @@ class Img extends Skin
             var scaleX = w.w / bmp.width;
             var scaleY = w.h / bmp.height;
 
-			if (keepAspect) {
-				scaleX = scaleY = (this.crop ? Math.max(scaleX, scaleY) : Math.min(scaleX, scaleY));
-				matrix.scale (scaleX, scaleY);
-				matrix.translate(
-					(w.w - bmp.width * scaleX) * 0.5,
-					(w.h - bmp.height * scaleY) * 0.5
-				);
-			} else {
-				matrix.scale(scaleX, scaleY);
-			}
+            if (keepAspect) {
+                scaleX = scaleY = (this.crop ? Math.max(scaleX, scaleY) : Math.min(scaleX, scaleY));
+                matrix.scale (scaleX, scaleY);
+                matrix.translate(
+                    (w.w - bmp.width * scaleX) * 0.5,
+                    (w.h - bmp.height * scaleY) * 0.5
+                );
+            } else {
+                matrix.scale(scaleX, scaleY);
+            }
 
-			w.graphics.beginBitmapFill(bmp, matrix, false, this.smooth);
-			if (this.crop) {
-				w.graphics.drawRect(0, 0, w.w, w.h);
-			} else {
-				w.graphics.drawRect(matrix.tx, matrix.ty, bmp.width * scaleX, bmp.height * scaleY);
-			}
-			w.graphics.endFill();
-		}
-	}
+            w.graphics.beginBitmapFill(bmp, matrix, false, this.smooth);
+            if (this.crop) {
+                w.graphics.drawRect(0, 0, w.w, w.h);
+            } else {
+                w.graphics.drawRect(matrix.tx, matrix.ty, bmp.width * scaleX, bmp.height * scaleY);
+            }
+            w.graphics.endFill();
+        }
+    }
 	#end
 
 
@@ -222,45 +238,45 @@ class Img extends Skin
 *******************************************************************************/
 
 
-	/**
-	* Getter src
-	*
-	*/
-	private inline function get_src() : String {
-		return this._src;
-	}//function get_src()
+    /**
+    * Getter src
+    *
+    */
+    private inline function get_src() : String {
+        return this._src;
+    }//function get_src()
 
 
-	/**
-	* Setter src
-	*
-	*/
-	private inline function set_src(src:String) : String {
-		if( src != null ){
-			this._bitmapData = null;
-		}
-		return this._src = src;
-	}//function set_src()
+    /**
+    * Setter src
+    *
+    */
+    private inline function set_src(src:String) : String {
+        if( src != null ){
+            this._bitmapData = null;
+        }
+        return this._src = src;
+    }//function set_src()
 
 
-	/**
-	* Getter bitmapData
-	*
-	*/
-	private inline function get_bitmapData() : BitmapData {
-		return this._bitmapData;
-	}//function get_bitmapData()
+    /**
+    * Getter bitmapData
+    *
+    */
+    private inline function get_bitmapData() : BitmapData {
+        return this._bitmapData;
+    }//function get_bitmapData()
 
 
-	/**
-	* Setter bitmapData
-	*
-	*/
-	private inline function set_bitmapData(bitmapData:BitmapData) : BitmapData {
-		if( bitmapData != null ){
-			this._src = null;
-		}
-		return this._bitmapData = bitmapData;
-	}//function set_bitmapData()
+    /**
+    * Setter bitmapData
+    *
+    */
+    private inline function set_bitmapData(bitmapData:BitmapData) : BitmapData {
+        if( bitmapData != null ){
+            this._src = null;
+        }
+        return this._bitmapData = bitmapData;
+    }//function set_bitmapData()
 
 }//class Img
